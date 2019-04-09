3593b39d5923fc38e9606deb6904dcd27f79203c
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-04-09 19:56:31 -0400

    add main()

--- src/test.cpp
+++ src/test.cpp
@@ -30,3 +30,9 @@ TEST(Tape, build_tape_file_name)
 	build_tape_file_name(NULL, outstr);
 	EXPECT_STREQ(outstr, "      ");
 }
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
