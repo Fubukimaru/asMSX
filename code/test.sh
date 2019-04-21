a576aec4353a329323686822bcb76330f0ca2f22
Alberto Gutiérrez Torre <agutierrez@posteo.net>
2019-04-21 19:38:38 +0200

    wrally as debug code

--- code/test.sh
+++ code/test.sh
@@ -9,9 +9,15 @@ $AS mine.asm
 $AS pong.asm
 $AS robots.asm
 $AS zone.asm
+
+cd wrally
+../$AS src/rally.asm 
+mv src/rally.z80 ../rally.rom
+cd ..
+
+
 rm -f ~* *.txt *.sym
 sha1sum -c test.sha1 > test.output 2> test.error
 
-
-cat test.error
 cat test.output
+cat test.error
