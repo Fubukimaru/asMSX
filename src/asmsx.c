b0a10e62dfdb401b1e0ed1d6d4194186876c523c
Fubu <agutierrez@posteo.net>
2020-12-20 16:04:59 +0100

    Added replace word function

--- src/asmsx.c
+++ src/asmsx.c
@@ -639,3 +639,47 @@ char *safe_strcat(char *dest, char *orig, unsigned int max_size,
   strcat(dest, orig);
   return (dest);
 }
+
+// https://www.geeksforgeeks.org/c-program-replace-word-text-another-given-word/
+// C program to search and replace 
+// all occurrences of a word with 
+// other word. 
+  
+// Function to replace a string with another 
+// string 
+char* replaceWord(const char* s, const char* oldW, const char* newW) { 
+    char* result; 
+    int i, cnt = 0; 
+    int newWlen = strlen(newW); 
+    int oldWlen = strlen(oldW); 
+  
+    // Counting the number of times old word 
+    // occur in the string 
+    for (i = 0; s[i] != '\0'; i++) { 
+        if (strstr(&s[i], oldW) == &s[i]) { 
+            cnt++; 
+  
+            // Jumping to index after the old word. 
+            i += oldWlen - 1; 
+        } 
+    } 
+  
+    // Making new string of enough length 
+    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1); 
+  
+    i = 0; 
+    while (*s) { 
+        // compare the substring with the result 
+        if (strstr(s, oldW) == s) { 
+            strcpy(&result[i], newW); 
+            i += newWlen; 
+            s += oldWlen; 
+        } 
+        else
+            result[i++] = *s++; 
+    } 
+  
+    result[i] = '\0'; 
+    return result; 
+} 
+ 
